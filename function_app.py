805b88af67693efae85cd551c8f7725f8b0af628
PhilippTrashman <p.altmann@hotmail.de>
2024-04-15 15:46:17 +0200

    first api request, route cannot be empty

--- function_app.py
+++ function_app.py
@@ -3,4 +3,15 @@ import datetime
 import json
 import logging
 
+import sys
+import os
+
 app = func.FunctionApp()
+backend = None
+
+TOKEN = os.environ["TOKEN"]
+print(f"TOKEN: {TOKEN}")
+
+@app.route('hello', methods=['GET'], auth_level=func.AuthLevel.ANONYMOUS)
+def get_hello_world(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(f"Hello World")
